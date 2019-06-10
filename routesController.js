58b52bf04071e192b28cad16bdb35eba71999474
ciscomar <47339599+ciscomar@users.noreply.github.com>
2019-06-10 10:33:43 -0600

    login, effects

--- routesController.js
+++ routesController.js
@@ -8,14 +8,17 @@ const funcionE = require('./public/js/empleadosFunctions');
 // Index GET
 controller.index_GET = (req, res) => {
 
-    funcion.controllerCargarAnuncios((err, result) => {
-        if (err) throw err;
+    funcionE.empleadosDirectorio((err, directorio) => {
+
+        funcion.controllerCargarAnuncios((err, result) => {
+            if (err) throw err;
 
 
             res.render('index.ejs', {
-                data: result
+                data: result, data2: directorio
             });
         });
+    });
 };
 
 
@@ -28,8 +31,16 @@ controller.alta_anuncio_GET = (req, res) => {
 //Login
 controller.login = (req, res) => {
     loginId = req.params.id
-    res.render('login.ejs', {
-        data: loginId
+
+    if (loginId == 'cambiar_directorio') {
+        next = '';
+    } else {
+        next = '/tipo';
+    }
+    funcionE.empleadosRevisarAccess1((err, result) => {
+        res.render('login.ejs', {
+            data: loginId, data2: result, data3: next
+        });
     });
 };
 
@@ -37,77 +48,49 @@ controller.login = (req, res) => {
 //POST a alta_anuncio despues de login primero revisa si el Gafete existe 
 controller.alta_anuncio_tipo_POST = (req, res) => {
     numeroEmpleado = req.body.user;
-    
-    funcionE.empleadosRevisarAccess1(numeroEmpleado, (err, count) => {
-        if (err) {
-            res.redirect('/login/alta_anuncio')
-        } else {
-            if (count == 0) {
-                res.redirect('/login/alta_anuncio')
-            }
-            else {
-                funcionE.empleadosNombre(numeroEmpleado, (err, result1) => {
-                    if (err) throw err;
-                    //Variables
 
-                    nombreEmpleado = result1
-                    res.render('tipo.ejs', {
-                        data: { numeroEmpleado, nombreEmpleado }
-                    });
-                });
-            }
-        }
+    funcionE.empleadosNombre(numeroEmpleado, (err, result1) => {
+        if (err) throw err;
+        //Variables
+
+        nombreEmpleado = result1
+        res.render('tipo.ejs', {
+            data: { numeroEmpleado, nombreEmpleado }
+        });
     });
+
 };
 
 //POST a alta_anuncio despues de login primero revisa si el Gafete existe 
 controller.alta_anuncio_anuncio_POST = (req, res) => {
     numeroEmpleado = req.body.gafete;
-    funcionE.empleadosRevisarAccess1(numeroEmpleado, (err, count) => {
-        if (err) {
-            res.redirect('/login/alta_anuncio')
-        } else {
-            if (count == 0) {
-                res.redirect('/login/alta_anuncio')
-            }
-            else {
-                funcionE.empleadosNombre(numeroEmpleado, (err, result1) => {
-                    if (err) throw err;
-                    //Variables
 
-                    nombreEmpleado = result1
-                    res.render('alta_anuncio.ejs', {
-                        data: { numeroEmpleado, nombreEmpleado }
-                    });
-                });
-            }
-        }
+    funcionE.empleadosNombre(numeroEmpleado, (err, result1) => {
+        if (err) throw err;
+        //Variables
+
+        nombreEmpleado = result1
+        res.render('alta_anuncio.ejs', {
+            data: { numeroEmpleado, nombreEmpleado }
+        });
     });
+
 };
 
 //POST a alta_anuncio despues de login primero revisa si el Gafete existe 
 controller.alta_anuncio_archivo_POST = (req, res) => {
     numeroEmpleado = req.body.gafete;
-    funcionE.empleadosRevisarAccess1(numeroEmpleado, (err, count) => {
-        if (err) {
-            res.redirect('/login/alta_anuncio')
-        } else {
-            if (count == 0) {
-                res.redirect('/login/alta_anuncio')
-            }
-            else {
-                funcionE.empleadosNombre(numeroEmpleado, (err, result1) => {
-                    if (err) throw err;
-                    //Variables
 
-                    nombreEmpleado = result1
-                    res.render('alta_archivo.ejs', {
-                        data: { numeroEmpleado, nombreEmpleado }
-                    });
-                });
-            }
-        }
+    funcionE.empleadosNombre(numeroEmpleado, (err, result1) => {
+        if (err) throw err;
+        //Variables
+
+        nombreEmpleado = result1
+        res.render('alta_archivo.ejs', {
+            data: { numeroEmpleado, nombreEmpleado }
+        });
     });
+
 };
 
 //POST a guardar_orden despues de crear orden2
@@ -134,13 +117,57 @@ controller.guardar_archivo_POST = (req, res) => {
     gafete = (req.body.gafete)
     pdf = (req.body.pdf)
     fecha = (Date())
-            
-        res.render('guardar_archivo.ejs', {
-            data: {  gafete, fecha, pdf }
-         
-            
+
+    res.render('guardar_archivo.ejs', {
+        data: { gafete, fecha, pdf }
+
+
+    });
+};
+
+//POST a cambiar_directorio
+controller.cambiar_directorio_POST = (req, res) => {
+
+    funcionE.empleadosDirectorio((err, result) => {
+        res.render('cambiar_directorio.ejs', {
+            data2: result
         });
-    };
-    
+    });
+};
+
+controller.cambiar_directorio2_POST = (req, res) => {
+
+extension=req.body.extension2
+nombre= req.body.nombre2
+
+
+//nombre=req.body.nombre
+        res.render('cambiar_directorio2.ejs', {
+            data: extension, data2: nombre
+        });
+
+};
+
+//POST a cambiar_directorio
+controller.guardar_directorio_POST = (req, res) => {
+    extension = req.body.extension
+    nombre = req.body.nombre;
+
+    funcionE.empleadosInsertDirectorio(extension, nombre, (err, result) => {
+
+        res.render('guardar_directorio.ejs', {
+            data: { extension, nombre }
+
+        });
+    });
+};
+
+controller.eliminar_directorio_POST = (req, res) => {
+extension= req.body.extension3
+    funcionE.empleadosDeleteDirectorio(extension, (err, result) => {
+    res.redirect('/login/cambiar_directorio')
+    });
+};
+
 
 module.exports = controller;
