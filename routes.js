32433ba288bade1be3b7dcd4e065a7ca4cc41e1f
Guillen <Aram.Guillen@tristone.com>
2019-12-03 07:17:16 -0700

    .

--- routes.js
+++ routes.js
@@ -18,20 +18,18 @@ var upload = multer( { storage: storage } );
 
 //Routes
 
-router.get('/landing_page', routesController.index_GET);
-router.get('/landing_page/login/:id', routesController.login);
-router.get('/landing_page/alta_anuncio/login', routesController.alta_anuncio_GET);
-router.post('/landing_page/alta_anuncio/tipo', routesController.alta_anuncio_tipo_POST);
-router.post('/landing_page/alta_anuncio/anuncio', routesController.alta_anuncio_anuncio_POST);
-router.post('/landing_page/alta_anuncio/archivo', routesController.alta_anuncio_archivo_POST);
-router.post('/landing_page/guardar_anuncio',routesController.guardar_anuncio_POST);
-router.post('/landing_page/guardar_archivo', upload.single('/pdf') ,routesController.guardar_archivo_POST);
-router.post('/landing_page/cambiar_directorio',routesController.cambiar_directorio_POST);
-router.post('/landing_page/cambiar_directorio2',routesController.cambiar_directorio2_POST);
-router.post('/landing_page/guardar_directorio',routesController.guardar_directorio_POST);
-router.post('/landing_page/eliminar_directorio',routesController.eliminar_directorio_POST);
+router.get('/', routesController.index_GET);
+router.get('/login/:id', routesController.login);
+router.get('/alta_anuncio/login', routesController.alta_anuncio_GET);
+router.post('/alta_anuncio/tipo', routesController.alta_anuncio_tipo_POST);
+router.post('/alta_anuncio/anuncio', routesController.alta_anuncio_anuncio_POST);
+router.post('/alta_anuncio/archivo', routesController.alta_anuncio_archivo_POST);
+router.post('/guardar_anuncio',routesController.guardar_anuncio_POST);
+router.post('/guardar_archivo', upload.single('/pdf') ,routesController.guardar_archivo_POST);
+router.post('/cambiar_directorio',routesController.cambiar_directorio_POST);
+router.post('/cambiar_directorio2',routesController.cambiar_directorio2_POST);
+router.post('/guardar_directorio',routesController.guardar_directorio_POST);
+router.post('/eliminar_directorio',routesController.eliminar_directorio_POST);
 
-router.get('*', (req, res) => {
-  res.send('404 Page not foundddddssssddd');
-});
+
 module.exports = router;
